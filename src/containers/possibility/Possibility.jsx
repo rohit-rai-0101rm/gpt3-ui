c61e5a4f001e0fc870bb5ed3eb18a48777d27f8d
rohit rai <rohitrai.dev@gmail.com>
2021-12-07 13:15:13 +0530

    navbar in progress

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './possibility.css'
+const Possibilty = () => {
+    return (
+        <div>
+            possibility
+        </div>
+    )
+}
+
+export default Possibilty
