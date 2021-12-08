7ec32d694d0e6652fb0c224a49b10866125ac1fe
rohit rai <rohitrai.dev@gmail.com>
2021-12-08 07:56:57 +0530

    header and brand section done

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './features.css'
 const Features = () => {
     return (
-        <div>
-            features
+        <div className="gpt3__features section__padding" id="features">
+            <div className="gpt3__features-heading">
+            </div>
         </div>
     )
 }
